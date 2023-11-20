c03ddf1946a56da11cdbee089b6b63cb85eeae98
Lucille <82943831+luciadobre@users.noreply.github.com>
2023-11-20 09:51:59 +0200

    Initial commit

---
+++ resources/views/cute-pepe.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Cute Pepe</title>
+
+    </head>
+    <body>
+       <img src="https://static.displate.com/857x1200/displate/2022-12-30/3fed295a166902d4ecd0195139ebac57_9dffe1a2678d4130df990fbab118a472.jpg" alt="hoodie pepe">
+    </body>
+</html>
